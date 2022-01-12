9146dc7519a374c18c016a67c2b05d80f31d14fd
Rostyslav Marchuk <rostyslavmarchuk440@gmail.com>
2022-01-12 14:35:40 +0200

    Added general video-statistic initialization function

--- constants/collections.js
+++ constants/collections.js
@@ -1,5 +1,6 @@
 const APPLICATIONS = 'applications'
 const VIDEOS = 'videos'
 const SUBTITLES = 'subtitles'
+const VIDEO_STATISTICS = 'video-statistics'
 
-module.exports = { APPLICATIONS, VIDEOS, SUBTITLES }
+module.exports = { APPLICATIONS, VIDEOS, SUBTITLES, VIDEO_STATISTICS }
--- domains/Video/Video.controller.js
+++ domains/Video/Video.controller.js
@@ -68,9 +68,24 @@ class VideoController {
         .get()
       const filePath = fileData.data().path
       const posterPath = fileData.data().posterPath
+      // Video file delete from storage
       await admin.storage().bucket().file(filePath).delete()
+      // Poster file delete from storage
       await admin.storage().bucket().file(posterPath).delete()
+      // Video metadata document delete from firestore
       await dbService.deleteDocument(COLLECTIONS.VIDEOS, videoId)
+      // Video statistics delete from firestore
+      const videoStatisticQuery = await dbService
+        .getCollectionRef(COLLECTIONS.VIDEO_STATISTICS)
+        .where('videoInfo.videoId', '==', videoId)
+        .get()
+      const videoStatisticId = videoStatisticQuery.docs.map(
+        (doc) => doc.data().id
+      )[0]
+      await dbService.deleteDocument(
+        COLLECTIONS.VIDEO_STATISTICS,
+        videoStatisticId
+      )
       res.status(200).send({ data: 'deleted' })
     } catch (err) {
       res.status(501).send({ data: err.message })
